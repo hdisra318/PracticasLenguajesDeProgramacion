de68b1c06d92b9c09981c62330ae00faca1c6f1a
hdisra318 <hdisra318@ciencias.unam.mx>
2022-11-05 14:40:53 -0600

    Terminando Funcion 1

--- Practica04/practica04.rkt
+++ Practica04/practica04.rkt
@@ -36,24 +36,24 @@ Martinez Calzada Diego -  318275457
 ;; Tipo de dato para los ambientes (Environment)
 (define-type Environment
   [mtSub]
-  [aSub (name symbol?) (value AST?) (bSub Environment ?)])
+  [aSub (name symbol?) (value AST?) (bSub Environment?)])
 
 ;; Tipo de dato para los valores FWAEL
 (define-type FWAEL-Value
-  [numV (n number ?)]
+  [numV (n number?)]
   [boolV (b boolean?)]
   [listV (l (listof FWAEL-Value?))]
   [closureV (param (listof symbol?)) (body AST?) (env Environment?)])
 
 ;; ******************************************************************
 
-;; 1. (2 pts). Defina la función (parse sexp), la cual recibe una expresión simbólica (symbolic expression,
-;; s-expression); esto es, la expresión puede ser un símbolo o una lista de expresiones simbólicas. (parse sexp)
-;; debe construir un Árbol de Sintaxis Abstracta (Abstract Syntax Tree - AST) a partir de la s-expression dada; si
-;; es una expresión válida en el lenguaje FWAEL. En otro caso, debe arrojar un error.
-;; * Precondiciones: una expresión simbólica (symbolic expression; s-expression).
-;; * Postcondiciones: si la s-expression de entrada satisface la gramática del lenguaje FWAEL; devuelve un árbol
-;;   de sintaxis abstracta AST, de lo contrario, enviar un error.
+;; 1. (2 pts). Funcion que cual recibe una expresion simbolica (symbolic expression,
+;; s-expression); y construye un Arbol de Sintaxis Abstracta (Abstract Syntax Tree - AST) a
+;; partir de la s-expression dada; si es una expresión válida en el lenguaje FWAEL.
+;; En otro caso, arroja un error.
+;; * Precondiciones: una expresion simbolica (symbolic expression; s-expression).
+;; * Postcondiciones: si la s-expression de entrada satisface la gramática del lenguaje FWAEL;
+;;   devuelve un arbol de sintaxis abstracta AST, de lo contrario, envia un error.
 ;; parse: s-expression -> AST
 (define (parse sexp)
   (define (parse-op opsexp)
@@ -69,20 +69,32 @@ Martinez Calzada Diego -  318275457
                       [(not) not])])
       (op operador (map parse (rest opsexp)))
       )
-    
+  )
+  (define (parse-op-bool opsexp)
+    (let (
+          [operador (case (first opsexp)
+                      [(and) 'and]
+                      [(or) 'or])])
+      (op-bool operador (parse (second opsexp)) (parse (third opsexp)))
+      )
   )
   (cond
     [(symbol? sexp)
         (case sexp
           [(T) (bool #t)]
           [(F) (bool #f)]
+          [(lempty) (lempty)]
           [else (id sexp)]
           )]
     [(number? sexp) (num sexp)]
     [(list? sexp) (case (first sexp)
           [(+ - * / modulo expt not) (parse-op sexp)]
+          [(or and) (parse-op-bool sexp)]
           [(with) (with (map appBinding (second sexp)) (parse (third sexp)))]
           [(with*) (with* (map appBinding (second sexp)) (parse (third sexp)))]
+          [(lcons) (lcons (parse (second sexp)) (parse (third sexp)))]
+          [(lcar) (lcar (parse (second sexp)))]
+          [(lcdr) (lcdr (parse (second sexp)))]
           [(fun) (fun (second sexp) (funBody (third sexp)))]
           [(app)
            (if (equal? 'fun (first (second sexp)))
@@ -95,20 +107,83 @@ Martinez Calzada Diego -  318275457
   )
 
 
+;; Funcion auxiliar de parse que transforma a binding la pareja pasada
+(define (appBinding pareja)
+  (binding (first pareja) (parse (second pareja))))
+
+;; Funcion auxiliar de parse que realiza el parse del cuerpo de la funcion
+(define (funBody body)
+  (if (empty? (rest body))
+              (parse (first body))
+              (parse body)))
 
 
-;; 2
 
-;; 3
+;; ******************************************************************
 
-;; 4
+;; 2. (3 pts). Funcion (desugar s-fwael-expr) que elimina el azucar sintactica de la expresion FWAEL
+;; dada; la cual se encuentra en forma de Arbol de Sintaxis Abstracta, y devuelve un nuevo arbol FWAEL
+;; sin azucar sintactica.
+;; * Precondiciones: Una expresion FWAEL en su representacion como AST.
+;; * Postcondiciones: Una expresión FWAEL sin azucar sintáctica representada en un AST.
+;;   desugar: AST -> AST
 
-;;   INTERP
 
+
+
+
+;; ******************************************************************
+
+;; 3. (1 pts). Funcion que realiza la sustitucion del parametro formal sub-id de la funcion de
+;; la que la expresion FWAEL sin azúcar sintactica que forma parte de su cuerpo, por el
+;; parametro real que debería encontrarse en el ambiente (Environment) env. En caso de no
+;; encontrar el simbolo sub-id que representa el parametro formal en el ambiente env,
+;; envia un error.
+;; * Precondiciones: Una expresion FWAEL sin azucar sintactica representada en un AST y
+;;   un ambiente.
+;; * Postcondiciones: Una expresion FWAEL sin azucar sintactica representada como AST
+;;   donde se ha sustituido el parametro formal de la expresion del cuerpo de la funcion
+;;   dada por el parámetro real que deberia estar en el ambiente. En caso de que el
+;;   parametro formal no se encuentre en el ambiente, envia un error.
+;; subst: AST, Environment -> AST
+
+
+
+
+
+;; ******************************************************************
+
+;; 4. (3 pts). Funcion que evalua una expresion FWAEL sin azucar sintactica
+;; dada bajo el ambiente (Environment) env dado. El resultado de la evaluacion debe ser un valor FWAEL
+;; (FWAEL-Value) y no un valor de Racket. Para evaluar expresiones fun es necesario utilizar la funcion subst.
+;; * Precondiciones: una expresion FWAEL en su representacion como Arbol de Sintaxis Abstracta (AST), sin azucar
+;;   sintactica y un ambiente de ejecucion.
+;; * Postcondiciones: un valor FWAEL al que se evalua la expresion en el ambiente dado.
+;; interp: AST, Environment -> FWAEL-Value
 (define (interp fwael-expr env)
   (cond
-    [(id? fwael-expr) #f]
-    ;; TODO LO DEMAS
+    ;;[(id? fwael-expr) #f]
+    [(id? fwael-expr) (error "error: Variable libre")]
+    [(num? fwael-expr) (num-n fwael-expr)]
+    [(bool? fwael-expr) (bool-b fwael-expr)]
+    [(op? fwael-expr) (cond
+                      [(eq? + (op-f fwael-expr)) (operSum (map interp (op-args fwael-expr)))]
+                      [(eq? - (op-f fwael-expr)) (operRes (map interp (op-args fwael-expr)))]
+                      [(eq? * (op-f fwael-expr)) (operProd (map interp (op-args fwael-expr)))]
+                      [(eq? / (op-f fwael-expr)) (operDiv (map interp (op-args fwael-expr)))]
+                      [(eq? modulo (op-f fwael-expr)) (operMod (map interp (op-args fwael-expr)))]
+                      [(eq? expt (op-f fwael-expr)) (operExpt (map interp (op-args fwael-expr)))]
+                      [(eq? not (op-f fwael-expr)) (operNot (map interp (op-args fwael-expr)))])]
+    [(with? fwael-expr) (let* (
+                             [bdgs (with-bindings fwael-expr)]
+                             [cuerpo (with-body fwael-expr)])
+                             (interp (evalWith cuerpo bdgs)))]
+    [(with*? fwael-expr) (let* (
+                             [bdgs (with*-bindings fwael-expr)]
+                             [cuerpo (with*-body fwael-expr)]
+                             [newBdgs (interpBdgs bdgs bdgs)])
+                             (interp (evalWith cuerpo newBdgs)))]
+    [(fun? fwael-expr) fwael-expr]
     [(app? fwael-expr) (let* (
         [apped-fun (app-fun fwael-expr)]
         [args (app-args fwael-expr)]
@@ -119,3 +194,103 @@ Martinez Calzada Diego -  318275457
     [else ("Error de sintaxis")]
   )
 )
+
+
+;; Funcion auxuliar de interp que realiza la operacion de suma sobre una lista de numeros.
+;; * Precondiciones: una lista de numeros de tamano mayor a 1.
+;; * Postcondiciones: la suma de los numeros de la lista, si la lista es de tamano menor a 1 se lanza
+;;   un error.
+(define (operSum l)
+  (if (> (length l) 1)
+      (foldl + 0 l)
+      (error "error: + requiere mas de un parametro")))
+
+;; Funcion auxuliar de interp que realiza la operacion de resta sobre una lista de numeros.
+;; * Precondiciones: una lista de numeros de tamano mayor a 1.
+;; * Postcondiciones: la resta de los numeros de la lista, si la lista es de tamano menor a 1 se lanza
+;;   un error.
+(define (operRes l)
+  (if (> (length l) 1)
+      (foldr - 0 l)
+      (error "error: - requiere mas de un parametro")))
+
+;; Funcion auxuliar de interp que realiza la operacion de producto sobre una lista de numeros.
+;; * Precondiciones: una lista de numeros de tamano mayor a 1.
+;; * Postcondiciones: el producto de los numeros de la lista, si la lista es de tamano menor a 1 se lanza
+;;   un error.
+(define (operProd l)
+  (if (> (length l) 1)
+      (foldl * 1 l)
+      (error "error: * requiere mas de un parametro")))
+
+;; Funcion auxuliar de interp que realiza la operacion de division sobre una lista de numeros.
+;; * Precondiciones: una lista de numeros de tamano 2.
+;; * Postcondiciones: la division de los numeros de la lista, si la lista es de tamano distinto de 2 se lanza
+;;   un error.
+(define (operDiv l)
+  (if (eq? (length l) 2)
+      (foldr / 1 l)
+      (error "error: / requiere dos parametros")))
+
+;; Funcion auxuliar de interp que realiza la operacion de modulo sobre una lista de numeros
+;; * Precondiciones: una lista de numeros de tamano 2.
+;; * Postcondiciones: el modulo de los numeros de la lista, si la lista es de tamano distinto de 2 se lanza
+;;   un error.
+(define (operMod l)
+  (if (eq? (length l) 2)
+      (modulo (first l) (second l))
+      (error "error: Modulo requiere dos parametros")))
+
+;; Funcion auxuliar de interp que realiza la operacion de expt sobre una lista de numeros
+;; * Precondiciones: una lista de numeros de tamano 2.
+;; * Postcondiciones: el expt de los numeros de la lista, si la lista es de tamano distinto de 2 se lanza
+;;   un error.
+(define (operExpt l)
+  (if (eq? (length l) 2)
+      (expt (first l) (second l))
+      (error "error: Expt requiere dos parametros")))
+
+;; Funcion auxuliar de interp que realiza la operacion de not sobre una lista de booleanos
+;; * Precondiciones: una lista de booleanos de tamano 1.
+;; * Postcondiciones: la negacion del booleano de la lista, si la lista es de tamano mayor a 1 se lanza
+;;   un error.
+(define (operNot l)
+  (if (eq? (length l) 1)
+      (not (first l))
+      (error "error: Not requiere un parametro")))
+
+;; Funcion auxiliar de interp que evalua el with
+(define (evalWith expr bdgs)
+  (if (eq? 0 (length bdgs))
+      expr
+      (evalWith (subst expr (binding-id (car bdgs)) (binding-value (car bdgs))) (rest bdgs))))
+
+;; Funcion auxiliar de interp que sustituye los ids en el resto de la lista de identificadores
+(define (interpBdgs bdgs bdgsAux)
+  (if (eq? 1 (length bdgsAux))
+      bdgs
+      (cons (car bdgs) (interpBdgs (interpBdg (car bdgs) (cdr bdgs)) (rest bdgsAux)))))
+
+;; Funcion auxiliar de interpBdgs que sustituye el binding dado en la lista de indentificadores
+(define (interpBdg bdg body)
+  (if(eq? 0 (length body))
+     body
+     (cons (substBdg bdg (car body)) (interpBdg bdg (rest body)))))
+
+;; Funcion auxiliar de interpBdg que hace el subst de un binding a otro binding
+(define (substBdg bdg1 bdg2)
+  (binding (binding-id bdg2) (subst (binding-value bdg2) (binding-id bdg1) (binding-value bdg1))))
+
+;; Funcion auxiliar de interp que evalua la funcion dados los parametros y los argumentos
+(define (evaluaFunc params oper argus)
+  (cond
+    [(not (eq? (length params) (length argus))) (error "error: El numero de parametros es inadecuado")]
+    [else (interp (subst-varios oper params argus))]
+   )
+)
+
+;; Funcion auxiliar de evaluaFunc para sustituir los valores de los parametros y evaluar la funcion
+(define (subst-varios oper ids valores)
+  (if (eq? 0 (length ids))
+      oper
+      (subst-varios (subst oper (first ids) (first valores)) (rest ids) (rest valores))))
